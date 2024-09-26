8b679ecfa6960f9ba7cd2f727a0c43f5dcc11815
Maren <113512118+maren-gonzalez@users.noreply.github.com>
2024-09-26 11:16:34 +0200

    applied health system (doesnt work)

--- Assignment3/Assets/Scripts/Damage.cs
+++ Assignment3/Assets/Scripts/Damage.cs
@@ -21,7 +21,7 @@ public class Damage : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         
-        if (other.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
             playerHealth.health -= damage;
         }
